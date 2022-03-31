48a63df951af684e43300b285f106a774cacd4de
Alexander Urlich <ulrichalex12@yahoo.de>
2022-04-01 00:05:10 +0200

    firebase yarn

--- App.js
+++ App.js
@@ -1,11 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, LogBox } from 'react-native';
+import { useState, useEffect } from 'react';
 const firebase = require('firebase');
 require('firebase/firestore');
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
-import { useEffect, useState } from 'react';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyAaBPzfQexglwOHAIgue96wHW2LXjOmAKs',
@@ -16,42 +12,42 @@ const firebaseConfig = {
   appId: '1:1011889322313:web:9a7608981a64eaacf08298',
 };
 
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
+LogBox.ignoreLogs(['Setting a timer']);
+
 export default function App() {
-  const [list, setList] = useState([]);
+  const [lists, setLists] = useState([]);
+
   useEffect(() => {
-    referenceShoppingLists = firebase.firestore().collection('shoppinglists');
-    if (
-      !referenceShoppingLists === null &&
-      !eferenceShoppingLists === undefined
-    ) {
-      unsubscribe = referenceShoppingLists.onSnapshot(onCollectionUpdate);
+    if (!firebase.apps.length) {
+      firebase.initializeApp(firebaseConfig);
     }
+
+    const referenceShoppingLists = firebase
+      .firestore()
+      .collection('ShoppingList');
+    const unsubscribe = referenceShoppingLists.onSnapshot(onCollectionUpdate);
     return () => {
       unsubscribe();
     };
-  });
+  }, []);
 
-  let list1 = firebase.firestore().collection('ShoppingList').doc('List1');
-
-  let onCollectionUpdate = (querySnapshot) => {
-    const lists = [];
+  const onCollectionUpdate = (querySnapshot) => {
+    const newLists = [];
     querySnapshot.forEach((doc) => {
       let data = doc.data();
-      lists.push({
+      newLists.push({
+        id: doc.id,
         name: data.name,
-        items: data.items.toString,
+        items: data.items.toString(),
       });
     });
-    setList(list);
+    setLists(newLists);
   };
 
   return (
     <View style={styles.container}>
       <FlatList
-        data={list}
+        data={lists}
         renderItem={({ item }) => (
           <Text>
             {item.name}: {item.items}
@@ -65,7 +61,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
